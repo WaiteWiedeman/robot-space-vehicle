a7a9e654d88d897d617f0326b8ec960ad720e4ca
WaiteWiedeman <119641244+WaiteWiedeman@users.noreply.github.com>
2025-03-28 17:52:36 -0400

    Add files via upload

--- robot_system.m
+++ robot_system.m
@@ -2,8 +2,8 @@ function dxdt = robot_system(t, x, sysParams, ctrlParams)
     [Xd, Yd, Xdd, Ydd, Xv, Xvd, Yv, Yvd] = referenceTrajectory(t, ctrlParams,sysParams);
     [Alv,Th1,Th2,Alvd,Om1,Om2] = InverseKinematics(Xd,Yd,Xdd,Ydd,Xv,Xvd,Yv,Yvd);
     if ctrlParams.noise
-        x = addnoise(t, x, ctrlParams);
-        F = force_function(t, x, Xv, Xvd, Yv, Yvd, Alv, Th1, Th2, Alvd, Om1, Om2, ctrlParams);
+        xm = addnoise(t, x, ctrlParams);
+        F = force_function(t, xm, Xv, Xvd, Yv, Yvd, Alv, Th1, Th2, Alvd, Om1, Om2, ctrlParams);
     else
         F = force_function(t, x, Xv, Xvd, Yv, Yvd, Alv, Th1, Th2, Alvd, Om1, Om2, ctrlParams);
     end
